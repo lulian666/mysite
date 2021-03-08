dbfb6db16bd0f32e0e96116b09ee5d48f9eb7fcd
lulian666 <644139490@qq.com>
2021-03-08 16:44:12 +0800

    自定义后台表单

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 10
+
+admin.site.register(Question, QuestionAdmin)
