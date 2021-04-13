267903a728743a066ef64082540fed793aed2225
lulian666 <644139490@qq.com>
2021-04-13 14:28:10 +0800

    优化后台展示

--- apitest/admin.py
+++ apitest/admin.py
@@ -25,14 +25,20 @@ class ApisAdmin(admin.TabularInline):
 
 admin.site.register(Apis)
 
-class HeadersAdmin(admin.TabularInline):
+class HeadersAdmin(admin.ModelAdmin):
     list_display = ['header_key', 'header_value']
+    list_filter = ['header_key']
+    search_fields = ['header_key']
+    list_per_page = 15
 
-class VariableAdmin(admin.TabularInline):
-    list_display = ['variable_key', 'variable_value']
+class VariableAdmin(admin.ModelAdmin):
+    list_display = ['variable_key', 'variable_value', 'from_api', 'Product_id']
+    list_filter = ['variable_key']
+    search_fields = ['variable_key']
+    list_per_page = 15
 
-admin.site.register(Headers)
-admin.site.register(Variables)
+admin.site.register(Headers, HeadersAdmin)
+admin.site.register(Variables, VariableAdmin)
 
 admin.site.site_header = 'AutotestPlat'
 admin.site.site_title = 'AutotestPlat'
