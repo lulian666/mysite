d156f822b8280fa838dfc304d5ea53a92c853f7a
lulian666 <644139490@qq.com>
2021-05-05 21:40:09 +0800

    后台注册文章模块

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from article.models import ArticleColumn
+
+
+class ArticleColumnAdmin(admin.ModelAdmin):
+    list_filter = ['column']
+    list_display = ['column', 'created', 'user']
+
+
+admin.site.register(ArticleColumn, ArticleColumnAdmin)
