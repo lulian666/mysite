15b486719b70ca0118f82bbe881327bc928d122d
lulian666 <644139490@qq.com>
2021-05-05 21:13:13 +0800

    编辑栏目名称

--- article/urls.py
+++ article/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'article'
 urlpatterns = [
     path('article-column/', views.article_column, name='article_column'),
+    path('rename-article-column/', views.rename_article_column, name='rename_article_column'),
 ]
--- article/views.py
+++ article/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 from article.forms import ArticleColumnForm
 from article.models import ArticleColumn
@@ -27,3 +28,19 @@ def article_column(request):
             ArticleColumn.objects.create(user=request.user, column=column_name)
             return HttpResponse('1')
 
+
+@login_required(login_url='/account/login/')
+@csrf_exempt
+@require_POST
+def rename_article_column(request):
+    print(request.POST)
+    column_name = request.POST['column_name']
+    column_id = request.POST['column_id']
+    print(column_id)
+    try:
+        line = ArticleColumn.objects.get(id=column_id)
+        line.column = column_name
+        line.save()
+        return HttpResponse('1')
+    except:
+        return HttpResponse('0')
