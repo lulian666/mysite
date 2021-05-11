b34e5105c238cb1dd461d548acd1711b98682c18
lulian666 <644139490@qq.com>
2021-05-11 14:37:26 +0800

    根据项目和测试状态来选择测试用例

--- apitest/views.py
+++ apitest/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from django.contrib.auth import authenticate, login
 
-
 # Create your views here.
 from django.urls import reverse
 
@@ -40,24 +39,24 @@ def login(request):
             return render(request, 'apitest/login.html', {'error': 'username or password error'})
     return render(request, 'apitest/login.html')
 
+
 @login_required
 def home(request):
     context = {}
-    context['username'] = request.session.get('user','')
+    context['username'] = request.session.get('user', '')
     return render(request, 'apitest/home.html', context)
-
-
 
 
 def logout(request):
     return render(request, 'apitest/login.html')
 
+
 @login_required
 def apitest_manage(request):
     apitest_list = Apitest.objects.all()
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     paginator = Paginator(apitest_list, 8)
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     currentPage = int(page)
     apitest_count = Apitest.objects.all().count()
     try:
@@ -66,14 +65,16 @@ def apitest_manage(request):
         apitest_list = paginator.page(1)
     except EmptyPage:
         apitest_list = paginator.page(paginator.num_pages)
-    return render(request, "apitest/apitest_manage.html", {"user":username, "apitests": apitest_list, "apitestcounts": apitest_count})
+    return render(request, "apitest/apitest_manage.html",
+                  {"user": username, "apitests": apitest_list, "apitestcounts": apitest_count})
+
 
 @login_required
 def apistep_manage(request):
     apistep_list = Apistep.objects.all()
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     paginator = Paginator(apistep_list, 8)
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     currentPage = int(page)
     apistep_count = Apistep.objects.all().count()
     try:
@@ -82,45 +83,47 @@ def apistep_manage(request):
         apistep_list = paginator.page(1)
     except EmptyPage:
         apistep_list = paginator.page(paginator.num_pages)
-    return render(request, "apitest/apistep_manage.html", {"user":username, "apisteps": apistep_list, "apistepcounts": apistep_count})
+    return render(request, "apitest/apistep_manage.html",
+                  {"user": username, "apisteps": apistep_list, "apistepcounts": apistep_count})
+
 
 @login_required
 def apis_manage(request):
-    if 'birth' in request.POST:
-        # 这里要生成case了哦！
-        Manage_sql().deleteCaseInSQL()
-        variable_list = Manage_sql().getVariablesFromSQL()
+    # 这一段一时想不起来是干什么用的，并且也没找到任何地方调用这里，先注释掉
+    # if 'birth' in request.POST:
+    #     # 这里要生成case了哦！
+    #     Manage_sql().deleteCaseInSQL()
+    #     variable_list = Manage_sql().getVariablesFromSQL()
+    #
+    #     basic_case_list, case_list = Case_collect().collect_data()
+    #     case_list = Case_ready(case_list, variable_list).data_form()
+    #
+    #     Manage_sql().deleteCaseInSQL()
+    #     Manage_sql().writeCaseToSQL(case_list)
 
-        basic_case_list, case_list = Case_collect().collect_data()
-        print(type(case_list))
-        case_list = Case_ready(case_list, variable_list).data_form()
-        for case in case_list:
-            print('case are ready:',case)
-
-        Manage_sql().deleteCaseInSQL()
-        Manage_sql().writeCaseToSQL(case_list)
-
-    username = request.session.get('user','')
-    productList = Product.objects.all()
+    test_result_list = [0, 1]  # {"0": "测试不通过","1": "测试通过"}
+    product_list = Product.objects.all()
     apis_list = Apis.objects.all()
-    paginator = Paginator(apis_list, 6)
-    page = request.GET.get('page',1)
-    currentPage = int(page)
-    apis_count = Apis.objects.all().count()
-    try:
-        apis_list = paginator.page(page)
-    except PageNotAnInteger:
-        apis_list = paginator.page(1)
-    except EmptyPage:
-        apis_list = paginator.page(paginator.num_pages)
-    return render(request, "apitest/apis_manage.html", {"user": username, "apiss": apis_list, "apicounts": apis_count, "products": productList})
+    apis_count, apis_page_list = paginator(request, Apis, apis_list, 6)
+
+    if 'filter' in request.POST:
+        apis_list_filtered, selected_test_result, selected_product_id = list_filter(request, apis_list)
+        return render(request, 'apitest/apis_manage.html',
+                      {'api_list': apis_list_filtered, "product_list": product_list,
+                       'test_result_list': test_result_list, "selected_test_result": selected_test_result,
+                       'selected_product_id': selected_product_id, 'apis_count': apis_list_filtered.count()})
+
+    return render(request, "apitest/apis_manage.html",
+                  {"api_list": apis_page_list, "apis_count": apis_count,
+                   "product_list": product_list, 'test_result_list': test_result_list})
+
 
 @login_required
 def test_report(request):
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     apis_list = Apis.objects.all()
     apis_count = Apis.objects.all().count()
-    db = pymysql.connect(user='root', db='dj',passwd='52france',host='127.0.0.1')
+    db = pymysql.connect(user='root', db='dj', passwd='52france', host='127.0.0.1')
     cursor = db.cursor()
     sql1 = 'SELECT count(id) FROM apitest_apis WHERE apitest_apis.apistatus=1'
     aa = cursor.execute(sql1)
@@ -129,60 +132,36 @@ def test_report(request):
     bb = cursor.execute(sql2)
     apis_fail_count = [row[0] for row in cursor.fetchmany(bb)][0]
     db.close()
-    return render(request, "apitest/report.html", {"user":username, "apiss": apis_list, "apiscounts": apis_count, "apis_pass_count":apis_pass_count, "apis_fail_count":apis_fail_count})
+    return render(request, "apitest/report.html",
+                  {"user": username, "apiss": apis_list, "apiscounts": apis_count, "apis_pass_count": apis_pass_count,
+                   "apis_fail_count": apis_fail_count})
+
 
 def left(request):
     return render(request, "apitest/left.html")
 
+
 @login_required
 def search(request):
-    username = request.session.get('user','')
-    search_apitestname = request.GET.get("apitestname","")
+    username = request.session.get('user', '')
+    search_apitestname = request.GET.get("apitestname", "")
     apitest_list = Apitest.objects.filter(apitestname__icontains=search_apitestname)
     # apitest_list = Apitest.objects.filter(apitestname=search_apitestname)
 
-    return render(request,"apitest/apitest_manage.html",{"user":username,"apitests":apitest_list})
+    return render(request, "apitest/apitest_manage.html", {"user": username, "apitests": apitest_list})
+
 
 @login_required
 def apissearch(request):
-    username = request.session.get('user','')
-    apiname = request.GET.get("apiname","")
+    username = request.session.get('user', '')
+    apiname = request.GET.get("apiname", "")
     apis_list = Apis.objects.filter(apiname__icontains=apiname)
-    return render(request,"apitest/apis_manage.html",{"user":username,"apiss":apis_list})
+    return render(request, "apitest/apis_manage.html", {"user": username, "apiss": apis_list})
 
 
 def welcome(request):
     return render(request, "apitest/welcome.html")
 
-def test(request):
-    productId = request.POST.get('productId')
-    testResult = request.POST.get('testresult')
-    # testResult = True if testResult == 1 else False
-    print(productId)
-    print(testResult)
-    productList = Product.objects.all()
-    print(productList)
-
-    selectedProduct = "可不选"
-
-    selector = {"-1": "可不选",
-                "0": "测试不通过",
-                "1": "测试通过"}
-    selectedTestResult = selector.get(testResult)
-    # testResultValue = selector.
-    apisList = Apis.objects.all()
-    if productId != '-1':
-        apisList = apisList.filter(Product_id=productId)
-        selectedProduct = productList.get(id=productId)
-    if testResult == '-1':
-        apisList = apisList.filter(apistatus__isnull=True)
-        print('x')
-    else:
-        apisList = apisList.filter(apistatus=True if testResult == '1' else False)
-        # selectedTestResult = "测试不通过" if testResult == "0" else "测试通过"
-        print('y')
-    # print(len(apisList))
-    return render(request, 'apitest/apis_manage.html', {'apiss': apisList, "products": productList, "selectedProduct": selectedProduct, "selectedTestResult": selectedTestResult, "selector": selector})
 
 def testapi(request):
     print('i test.')
@@ -201,10 +180,10 @@ def testapi(request):
     Manage_sql().updateCaseToSQL(caselist)
     print('Done!')
 
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     apis_list = Apis.objects.all()
     paginator = Paginator(apis_list, 8)
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     currentPage = int(page)
     apis_count = Apis.objects.all().count()
     try:
@@ -213,11 +192,12 @@ def testapi(request):
         apis_list = paginator.page(1)
     except EmptyPage:
         apis_list = paginator.page(paginator.num_pages)
-    return render(request, "apitest/apis_manage.html", {"user": username, "apiss": apis_list,"apicounts": apis_count})
+    return render(request, "apitest/apis_manage.html", {"user": username, "apiss": apis_list, "apicounts": apis_count})
+
 
 # 处理源数据
 def datasource(request):
-    source = request.POST.get('source','').strip()
+    source = request.POST.get('source', '').strip()
     error = ''
     # 区分两个按钮
     if 'analysis' in request.POST:
@@ -226,7 +206,7 @@ def datasource(request):
         else:
             error = 'this is a legal json'
     elif 'save' in request.POST:
-        root = os.path.abspath('.') #获取当前工作目录路径
+        root = os.path.abspath('.')  # 获取当前工作目录路径
         if not check_json_format(source):
             error = 'not a legal json'
         elif source != "{}":
@@ -239,8 +219,8 @@ def datasource(request):
             basic_case_list, case_list = Case_collect().collect_data()
             variables_dict = {}
             for case in basic_case_list:
-                print('basic case:',case)  # 这个是还没有替换值的case
-                if case[3] != {}: #处理body的
+                print('basic case:', case)  # 这个是还没有替换值的case
+                if case[3] != {}:  # 处理body的
                     param_list = []
                     for num, keys in list(enumerate(case[3])):
                         if 'enum' not in case[3][keys]:
@@ -248,9 +228,9 @@ def datasource(request):
                             param_list.append(keys)
                     if len(param_list) >= 0:
                         print("param_list:", param_list)
-                        variables_dict.update({case[0]:param_list})
+                        variables_dict.update({case[0]: param_list})
                 elif case[2] != {}:
-                    #处理parameters的
+                    # 处理parameters的
                     param_list = []
                     for num, keys in list(enumerate(case[2])):
                         if 'enum' not in case[2][keys]:
@@ -258,7 +238,7 @@ def datasource(request):
                             param_list.append(keys)
                     if len(param_list) >= 0:
                         print("param_list:", param_list)
-                        variables_dict.update({case[0]:param_list})
+                        variables_dict.update({case[0]: param_list})
             Manage_sql().deleteVariablesInSQL()
             Manage_sql().writeVariablesToSQL(2, variables_dict)
             # Manage_sql().deleteCaseInSQL()
@@ -266,7 +246,7 @@ def datasource(request):
         else:
             error = 'this is empty!!'
 
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     apis_list = Apis.objects.all()
     paginator = Paginator(apis_list, 8)
     page = request.GET.get('page', 1)
@@ -279,11 +259,13 @@ def datasource(request):
     except EmptyPage:
         apis_list = paginator.page(paginator.num_pages)
     # print("source:", source)
-    return render(request, "apitest/datasource_manage.html", {'error': error, 'data': source, "user": username, "apiss": apis_list,"apicounts": apis_count})
+    return render(request, "apitest/datasource_manage.html",
+                  {'error': error, 'data': source, "user": username, "apiss": apis_list, "apicounts": apis_count})
+
 
 # header 管理
 def api_header(request):
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     headers_list = Headers.objects.all()
     paginator = Paginator(headers_list, 12)
     page = request.GET.get('page', 1)
@@ -295,11 +277,13 @@ def api_header(request):
         headers_list = paginator.page(1)
     except EmptyPage:
         headers_list = paginator.page(paginator.num_pages)
-    return render(request, "apitest/api_header.html", {"user": username, "headers": headers_list, "apicounts": headers_count})
+    return render(request, "apitest/api_header.html",
+                  {"user": username, "headers": headers_list, "apicounts": headers_count})
+
 
 # 变量管理
 def variables_manage(request):
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     variables_list = Variables.objects.all()
     paginator = Paginator(variables_list, 12)
     page = request.GET.get('page', 1)
@@ -312,20 +296,64 @@ def variables_manage(request):
     except EmptyPage:
         variables_list = paginator.page(paginator.num_pages)
 
-    return render(request, "apitest/variables_manage.html", {"user": username, "variables": variables_list, "variablecounts": variables_count, "warning": "只点击一次就好，会跳转到用例列表"})
+    return render(request, "apitest/variables_manage.html",
+                  {"user": username, "variables": variables_list, "variablecounts": variables_count,
+                   "warning": "只点击一次就好，会跳转到用例列表"})
+
 
 def check_json_format(raw_msg):
     """
     用于判断一个字符串是否符合Json格式
-    :param self:
+    :param raw_msg:
     :return:
     """
     if isinstance(raw_msg, str) and not raw_msg.isdigit():
         raw_msg.strip()
         try:
-          json.loads(raw_msg)
+            json.loads(raw_msg)
         except ValueError:
             return False
         return True
     else:
         return False
+
+
+def paginator(request, model, page_list, number):
+    """
+    分页函数
+    :param request:
+    :param model:
+    :param page_list: 需要分页的list
+    :param number: 一页分多少
+    :return: 列表的总数和对应页数的list
+    """
+    paginator = Paginator(page_list, number)
+    page = request.GET.get('page', 1)
+    list_count = model.objects.all().count()
+
+    try:
+        page_list = paginator.page(page)
+    except PageNotAnInteger:
+        page_list = paginator.page(1)
+    except EmptyPage:
+        page_list = paginator.page(paginator.num_pages)
+
+    return list_count, page_list
+
+
+def list_filter(request, list_to_filter):
+    """
+    根据项目id和测试结果来过滤用例列表
+    :param request:
+    :param list_to_filter:
+    :return:
+    """
+    selected_product_id = request.POST.get('product_id')
+    selected_test_result = request.POST.get('test_result')
+    list_filtered = list_to_filter
+
+    if selected_product_id != '-1':
+        list_filtered = list_to_filter.filter(Product_id=selected_product_id)
+    if selected_test_result != '-1':
+        list_filtered = list_to_filter.filter(apistatus=True if selected_test_result == '1' else False)
+    return list_filtered, int(selected_test_result), int(selected_product_id)
