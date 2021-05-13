77daaeb7b1f6ee923d8bfa76ec1be653877454e9
lulian666 <644139490@qq.com>
2021-05-13 12:05:06 +0800

    理了一下发送测试类

--- apitest/common/case_test.py
+++ apitest/common/case_test.py
@@ -1,5 +1,4 @@
 # coding:utf-8
-
 import os
 import sys
 import time
@@ -10,76 +9,55 @@ from apitest.common.header_mange import HeaderManage
 from apitest.common.emailer import Email
 from apitest.common.reporter import Template_mixin
 
-direct = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(direct)
-sys.path.append('/usr/lib/python2.7/site-packages (2.22.0)')
-
 
 class CaseRequest:
     def __init__(self):
-        self.table_tr_fail = ''
-        self.table_tr_success = ''
+        self.table_tr_fail = self.table_tr_success = ''
 
-        self.num_success = 0
-        self.num_fail = 0
+        self.num_success = self.num_fail = 0
         self.html = Template_mixin()
 
     def send_request(self, case_list, host):
         # header 改从数据库里获取
         header = HeaderManage().readHeader(2)
         for case in case_list:
-            if case[2] == 'get' or case[2] == 'GET':
-                result = requests.get(host + case[1], headers=header, params=case[3], json=case[4])
-            elif case[2] == 'post' or case[2] == 'POST':
-                result = requests.post(host + case[1], json=case[4], headers=header)
+            result = request(case, host, header)
             if result.status_code == 401:
-                print('401了')
-                HeaderManage().updateHeader(2,host)
+                HeaderManage().updateHeader(2, host)
                 header = HeaderManage().readHeader(2)
-                if case[2] == 'get' or case[2] == 'GET':
-                    result = requests.get(host + case[1], headers=header, params=case[3], json=case[4])
-                else:
-                    result = requests.post(host + case[1], json=case[4], headers=header)
+                result = request(case, host, header)
 
-            # 下面是存报告信息的
+            # 存报告信息
             if result.status_code != case[5]:
-                apistatus = False
-                print('测试失败')
-                self.num_fail +=1
-                print('case:', case)
-                print('预期状态码',case[5])
-                print('实际状态码',result.status_code)
-                result_json = ''
+                api_status = False
+                self.num_fail += 1
                 try:
                     result_json = result.json()
                 except JSONDecodeError:
                     result_json = '由于异常，无法读取结果'
-                print('请求结果',result_json)
-                print()
                 btw = '可能的原因：\n' \
                       '1、某个参数没加required等于true\n' \
                       '2、参数类型传错了（请联系QA）\n' \
                       '3、真bug'
                 table_td = self.html.TABLE_TMPL_FAIL % dict(runtime=time.strftime('%Y-%m-%d %H:%M:%S'), interface=case[1],
                                                             method=case[2], parameters=case[3], body=case[4], expectcode=case[5],
-                                                            testresult='测试失败', testcode=result.status_code,resultbody=result_json,
+                                                            testresult='测试失败', testcode=result.status_code, resultbody=result_json,
                                                             btw=btw)
                 self.table_tr_fail += table_td
             else:
-                apistatus = True
-                print('测试成功')
-                self.num_success +=1
+                api_status = True
+                self.num_success += 1
                 table_td = self.html.TABLE_TMPL_SUCC % dict(runtime=time.strftime('%Y-%m-%d %H:%M:%S'), interface=case[1],
                                                             method=case[2], parameters=case[3], body=case[4], expectcode=case[5],
                                                             testresult='测试成功', testcode=result.status_code,)
                 self.table_tr_success += table_td
-            print('---------')
+
             # 存数据库信息
-            apiresponsestatuscode = result.status_code
-            apiresponse = "这里我有解决不了的问题，先放着"
-            case.append(apiresponsestatuscode)
-            case.append(apiresponse)
-            case.append(apistatus)
+            api_response_status_code = result.status_code
+            api_response = "这里我有解决不了的问题，先放着"
+            case.append(api_response_status_code)
+            case.append(api_response)
+            case.append(api_status)
         self.report()
         return case_list
 
@@ -91,7 +69,7 @@ class CaseRequest:
         filename = '{date}_TestReport.html'.format(date=time.strftime('%Y%m%d%H'))
 
         # 获取report的路径
-        root = os.path.abspath('.') #获取当前工作目录路径
+        root = os.path.abspath('.')
         filepath = os.path.join(root, 'apitest/report/'+filename)
 
         with open(filepath, 'wb') as f:
@@ -103,3 +81,12 @@ class CaseRequest:
 
     def get_num_fail(self):
         return self.num_fail
+
+
+def request(case, host, header):
+    if case[2] == 'get' or case[2] == 'GET':
+        result = requests.get(host + case[1], headers=header, params=case[3], json=case[4])
+    else:
+        result = requests.post(host + case[1], json=case[4], headers=header)
+    return result
+
