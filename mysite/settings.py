fa6e63e76d3718198fb34d01531b741c04729e12
lulian666 <644139490@qq.com>
2021-07-12 20:23:41 +0800

    修改登录成功后的跳转（因为主页地址变了）

--- mysite/settings.py
+++ mysite/settings.py
@@ -152,7 +152,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/apitest/home'
+LOGIN_REDIRECT_URL = '/'
 # LOGOUT_REDIRECT_URL = '/apitest/login/'
 
 EMAIL_HOST = 'smtp.gmail.com'
